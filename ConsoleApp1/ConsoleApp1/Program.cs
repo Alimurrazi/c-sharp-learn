e44ca93a470da8eb54e4d8dfa8e796d0340b8e84
Rana <alimur.razi@selise.ch>
2019-03-11 09:26:04 +0600

    initialized

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static string value = "Welcome";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Program.value);
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n);
+            add(ref n);
+            Console.WriteLine(n);
+            greet(3,"John", "Jonny", "jack");
+            Console.ReadLine();
+        }
+
+        static void add(ref int n)
+        {
+            n = n + 5;
+        }
+
+        static void greet(int n,params string[] names)
+        {
+            Console.WriteLine("total "+n);
+            foreach (string name in names)
+                Console.WriteLine("Hello ..."+name);
+        }
+
+    }
+}
