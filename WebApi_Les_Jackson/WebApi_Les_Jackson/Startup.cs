e797856ccf5f04f96839b3cda18974b1cbbbc9c3
Alimurrazi <alimurrazi570@gmail.com>
2019-12-10 09:33:41 +0600

    new wep api project add

--- WebApi_Les_Jackson/WebApi_Les_Jackson/Startup.cs
+++ WebApi_Les_Jackson/WebApi_Les_Jackson/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using WebApi_Les_Jackson.Models;
 
 namespace WebApi_Les_Jackson
 {
@@ -25,6 +27,9 @@ namespace WebApi_Les_Jackson
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommandContext>
+                (opt => opt.UseSqlServer(Configuration["Data:CommandAPIConnection:ConnectionString"]));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
