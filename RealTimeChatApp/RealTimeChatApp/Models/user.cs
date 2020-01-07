aaa787a8259b0ad9655a783dcf78703588ae2429
Alimurrazi <alimurrazi570@gmail.com>
2020-01-07 09:40:37 +0600

    user model add

---
+++ RealTimeChatApp/RealTimeChatApp/Models/user.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealTimeChatApp.Models
+{
+    public class user
+    {
+        public Guid id { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+        public string mail { get; set; }
+    }
+}
