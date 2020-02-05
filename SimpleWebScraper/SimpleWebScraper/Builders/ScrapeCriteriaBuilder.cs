169125ff266e0099cbf82584716c7d911e45250f
Alimurrazi <alimurrazi570@gmail.com>
2020-02-05 09:50:02 +0600

    two builders add

---
+++ SimpleWebScraper/SimpleWebScraper/Builders/ScrapeCriteriaBuilder.cs
@@ -0,0 +1,59 @@
+﻿using SimpleWebScraper.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SimpleWebScraper.Builders
+{
+    class ScrapeCriteriaBuilder
+    {
+        private string _data;
+        private string _regex;
+        private RegexOptions _regexOption;
+        private List<ScrapeCriteriaPart> _parts;
+
+        public ScrapeCriteriaBuilder()
+        {
+            setDefaults();
+        }
+        private void setDefaults()
+        {
+            _data = string.Empty;
+            _regex = string.Empty;
+            _regexOption = RegexOptions.None;
+            _parts = new List<ScrapeCriteriaPart>();
+        }
+
+        public ScrapeCriteriaBuilder withData(string data)
+        {
+            _data = data;
+            return this;
+        }
+        public ScrapeCriteriaBuilder withRegex(string regex)
+        {
+            _regex = regex;
+            return this;
+        }
+        public ScrapeCriteriaBuilder withRegexOption(RegexOptions regexOption)
+        {
+            _regexOption = regexOption;
+            return this;
+        }
+        public ScrapeCriteriaBuilder withPart(ScrapeCriteriaPart scrapeCriteriaPart)
+        {
+            _parts.Add(scrapeCriteriaPart);
+            return this;
+        }
+
+        public ScrapeCriteria Build()
+        {
+            ScrapeCriteria scrapeCriteria = new ScrapeCriteria();
+            scrapeCriteria.Data = _data;
+            scrapeCriteria.Regex = _regex;
+            scrapeCriteria.RegexOption = _regexOption;
+            scrapeCriteria.Parts = _parts;
+            return scrapeCriteria;
+        }
+    }
+}
