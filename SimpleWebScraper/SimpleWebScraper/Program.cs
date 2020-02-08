0cd208d1c82aed2a247ef5b10e594254d136f84d
Alimurrazi <alimurrazi570@gmail.com>
2020-02-08 09:58:46 +0600

    scraped elements with regex

--- SimpleWebScraper/SimpleWebScraper/Program.cs
+++ SimpleWebScraper/SimpleWebScraper/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using SimpleWebScraper.Builders;
+using SimpleWebScraper.Data;
+using SimpleWebScraper.Workers;
+using System;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace SimpleWebScraper
 {
@@ -7,6 +13,41 @@ namespace SimpleWebScraper
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            using (WebClient client = new WebClient())
+            {
+                string content = client.DownloadString("https://boston.craigslist.org/d/cars-trucks/search/cta");
+                Console.WriteLine(content);
+
+                ScrapeCriteria scrapeCriteria = new ScrapeCriteriaBuilder()
+                    .withData(content)
+                    .withRegex(@"<a href=\""(.*?)\"" data-id=\""(.*?)\"" class=\""result-title hdrlnk\"">(.*?)</a>")
+                    .withRegexOption(RegexOptions.ExplicitCapture)
+                    .withPart(new ScrapeCriteriaPartBuilder()
+                        .withRegex(@">(.*?)</a>")
+                        .withRegexOption(RegexOptions.Singleline)
+                        .Build())
+                    .withPart(new ScrapeCriteriaPartBuilder()
+                        .withRegex(@"href=\""(.*?)\""")
+                        .withRegexOption(RegexOptions.Singleline)
+                        .Build())
+                    .Build();
+
+                Scraper scraper = new Scraper();
+                var scrapedElements = scraper.Scrape(scrapeCriteria);
+                if (scrapedElements.Any())
+                {
+                    foreach(var scrapedElement in scrapedElements)
+                    {
+                        Console.WriteLine(scrapedElement);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("There are no match");
+                }
+            }
+
         }
     }
 }
