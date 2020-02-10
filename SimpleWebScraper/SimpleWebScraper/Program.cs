e4c26bc7633e9891f138b44ae157bd6c01e18f08
Alimurrazi <alimurrazi570@gmail.com>
2020-02-10 10:21:25 +0600

    scraped data fetched from website

--- SimpleWebScraper/SimpleWebScraper/Program.cs
+++ SimpleWebScraper/SimpleWebScraper/Program.cs
@@ -12,40 +12,46 @@ namespace SimpleWebScraper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
-            using (WebClient client = new WebClient())
+            try
             {
-                string content = client.DownloadString("https://boston.craigslist.org/d/cars-trucks/search/cta");
-                Console.WriteLine(content);
-
-                ScrapeCriteria scrapeCriteria = new ScrapeCriteriaBuilder()
-                    .withData(content)
-                    .withRegex(@"<a href=\""(.*?)\"" data-id=\""(.*?)\"" class=\""result-title hdrlnk\"">(.*?)</a>")
-                    .withRegexOption(RegexOptions.ExplicitCapture)
-                    .withPart(new ScrapeCriteriaPartBuilder()
-                        .withRegex(@">(.*?)</a>")
-                        .withRegexOption(RegexOptions.Singleline)
-                        .Build())
-                    .withPart(new ScrapeCriteriaPartBuilder()
-                        .withRegex(@"href=\""(.*?)\""")
-                        .withRegexOption(RegexOptions.Singleline)
-                        .Build())
-                    .Build();
-
-                Scraper scraper = new Scraper();
-                var scrapedElements = scraper.Scrape(scrapeCriteria);
-                if (scrapedElements.Any())
+                using (WebClient client = new WebClient())
                 {
-                    foreach(var scrapedElement in scrapedElements)
+                    string content = client.DownloadString("https://boston.craigslist.org/d/cars-trucks/search/cta");
+                    Console.WriteLine(content);
+
+                    ScrapeCriteria scrapeCriteria = new ScrapeCriteriaBuilder()
+                        .withData(content)
+                        .withRegex(@"<a href=\""(.*?)\"" data-id=\""(.*?)\"" class=\""result-title hdrlnk\"">(.*?)</a>")
+                        .withRegexOption(RegexOptions.ExplicitCapture)
+                        .withPart(new ScrapeCriteriaPartBuilder()
+                            .withRegex(@">(.*?)</a>")
+                            .withRegexOption(RegexOptions.Singleline)
+                            .Build())
+                        .withPart(new ScrapeCriteriaPartBuilder()
+                            .withRegex(@"href=\""(.*?)\""")
+                            .withRegexOption(RegexOptions.Singleline)
+                            .Build())
+                        .Build();
+
+                    Scraper scraper = new Scraper();
+                    var scrapedElements = scraper.Scrape(scrapeCriteria);
+                    if (scrapedElements.Any())
                     {
-                        Console.WriteLine(scrapedElement);
+                        foreach (var scrapedElement in scrapedElements)
+                        {
+                            Console.WriteLine(scrapedElement);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("There are no match");
                     }
                 }
-                else
-                {
-                    Console.WriteLine("There are no match");
-                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
             }
 
         }
