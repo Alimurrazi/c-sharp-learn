67c1323d1818ca5cfbca61d50995defbb122f81c
Alimurrazi <alimurrazi570@gmail.com>
2020-02-19 09:26:15 +0600

    database settings create

---
+++ RealTimeChatApp/RealTimeChatApp/Services/ChatService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealTimeChatApp.Services
+{
+    public class ChatService
+    {
+    }
+}
--- RealTimeChatApp/RealTimeChatApp/Startup.cs
+++ RealTimeChatApp/RealTimeChatApp/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using RealTimeChatApp.Models;
 
 namespace RealTimeChatApp
 {
@@ -30,6 +32,12 @@ namespace RealTimeChatApp
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+            services.Configure<DatabaseSettings>(
+                Configuration.GetSection(nameof(DatabaseSettings)));
+            
+            services.AddSingleton<DatabaseSettings>(sp =>
+            sp.GetRequiredService < IOptions < DatabaseSettings >> ().Value);
+
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
