60e4a75faef1cc8f548f5028268f0fd6058872fa
Alimurrazi <alimurrazi570@gmail.com>
2020-03-30 08:52:19 +0600

    swagger worked

--- Supermarket.API/Startup.cs
+++ Supermarket.API/Startup.cs
@@ -2,65 +2,68 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Supermarket.API.Persistence.Contexts;
-using Supermarket.API.Persistence.Repositories;
+using Microsoft.OpenApi.Models;
 using Supermarket.API.Domain.Repositories;
 using Supermarket.API.Domain.Services;
+using Supermarket.API.Persistence.Contexts;
+using Supermarket.API.Persistence.Repositories;
 using Supermarket.API.Services;
-using Microsoft.EntityFrameworkCore;
-using AutoMapper;
-namespace Supermarket.API
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace Supermarket.API {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-            services.AddDbContext<AppDbContext>(options => {
-                options.UseInMemoryDatabase("supermarket-api-in-memory");
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddControllers ();
+            services.AddDbContext<AppDbContext> (options => {
+                options.UseInMemoryDatabase ("supermarket-api-in-memory");
             });
 
-            services.AddScoped<ICategoryRepository, CategoryRepository>();
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped<ICategoryService, CategoryService>();
-            services.AddScoped<IProductService, ProductService>();
-            services.AddScoped<IUnitOfWork, UnitOfWork>();
-            services.AddAutoMapper(typeof(Startup));
+            services.AddScoped<ICategoryRepository, CategoryRepository> ();
+            services.AddScoped<IProductRepository, ProductRepository> ();
+            services.AddScoped<ICategoryService, CategoryService> ();
+            services.AddScoped<IProductService, ProductService> ();
+            services.AddScoped<IUnitOfWork, UnitOfWork> ();
+            services.AddAutoMapper (typeof (Startup));
+
+            services.AddSwaggerGen (c => {
+                c.SwaggerDoc ("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
-            app.UseHttpsRedirection();
+            app.UseHttpsRedirection ();
 
-            app.UseRouting();
+            app.UseRouting ();
 
-            app.UseAuthorization();
+            app.UseAuthorization ();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
+            });
+
+            app.UseSwagger ();
+            app.UseSwaggerUI (c => {
+                c.SwaggerEndpoint ("/swagger/v1/swagger.json", "My API V1");
             });
         }
     }
