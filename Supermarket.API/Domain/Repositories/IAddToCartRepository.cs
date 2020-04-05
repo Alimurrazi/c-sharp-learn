78494c683779a8bb08a79a6b8a877d506ec2025c
Alimurrazi <alimurrazi570@gmail.com>
2020-04-05 20:33:32 +0600

    find list of products by id not implemented

--- Supermarket.API/Domain/Repositories/IAddToCartRepository.cs
+++ Supermarket.API/Domain/Repositories/IAddToCartRepository.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Supermarket.API.Domain.Models;
 namespace Supermarket.API.Domain.Repositories
 {
     public interface IAddToCartRepository
     {
-        
+        Task<IEnumerable<Product>> GetProductListById(List<int> idList);
     }
 }
--- Supermarket.API/Persistence/Repositories/AddToCartRepository.cs
+++ Supermarket.API/Persistence/Repositories/AddToCartRepository.cs
@@ -1,8 +1,19 @@
 using Supermarket.API.Domain.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Supermarket.API.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Supermarket.API.Persistence.Contexts;
+
 namespace Supermarket.API.Persistence.Repositories
 {
-    public class AddToCartRepository : IAddToCartRepository
+    public class AddToCartRepository : BaseRepository, IAddToCartRepository
     {
-        
+        public AddToCartRepository(AppDbContext context) : base(context){
+        }
+       public async Task<IEnumerable<Product>> GetProductListById(List<int> idList){
+           
+           return await _context.Products.FindAsync(idList => id);
+        }
     }
 }
